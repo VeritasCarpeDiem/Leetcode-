39ad9e5088a2ac867255153c3a3840bc0b1fc86f
jinhong0425 <jinhong0425@gmail.com>
2021-12-21 23:51:29 -0800

    1st commit

---
+++ Leetcode/Easy/Array/26.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy.Array
+{
+    internal class Solution
+    {
+        public int removeDuplicates(int[] nums)
+        { 
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int i = 0;
+
+            for (int j = 1 ; j < nums.Length ; j++)
+            {
+                if(nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i]= nums[j];
+                }
+                
+            }
+
+            return i + 1;
+        }
+    }
+}
